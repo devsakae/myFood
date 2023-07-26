76793075a0b9e4f86016c2352904187830c09b00
Rodrigo Sakae <devsakae@gmail.com>
2023-07-26 08:29:20 -0300

    fix: problemas de segurança e novo cardápio em objeto mockado

--- src/components/Comidas/Disponiveis.jsx
+++ src/components/Comidas/Disponiveis.jsx
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Disponiveis.module.css";
 import Card from "../UI/Card";
 import Item from "./Item";
-import { useEffect } from "react";
-const FIREBASE_URL_FOODDB = 'https://myfood-3fc5a-default-rtdb.firebaseio.com/food.json';
+
+const cardapio = [
+  {
+    id: 1,
+    name: 'X-Salada',
+    description: 'Um delicioso X-Salada, completo com tudo que você precisa',
+    price: 28,
+  },
+  {
+    id: 2,
+    name: 'Porção de batatas fritas',
+    description: 'Uma porção de 400g de batatas fritas',
+    price: 18,
+  },
+  {
+    id: 3,
+    name: 'Skol garrafa de 1 litro',
+    description: 'Pra descer redondo com a comida',
+    price: 15,
+  },
+]
 
 export default function Disponiveis() {
   const [menu, setMenu] = useState([]);
@@ -12,20 +31,7 @@ export default function Disponiveis() {
 
   useEffect(() => {
     const fetchFood = async () => {
-      const response = await fetch(FIREBASE_URL_FOODDB);
-      if (!response.ok) {
-        throw new Error('Fetch failed');
-      }
-      const data = await response.json();
-      const loadedMeals = [];
-      for (const key in data) {
-        loadedMeals.push({
-          id: key,
-          name: data[key].name,
-          description: data[key].description,
-          price: data[key].price,
-        })
-      }
+      const loadedMeals = cardapio;
       setLoading(false);
       setMenu(loadedMeals);
     };
